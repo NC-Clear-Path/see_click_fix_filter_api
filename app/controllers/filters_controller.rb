04f34d76eb597c3547d27e00b659ddf6be9f0797
Crystal Williams Brown <crystal.williams.brown@gmail.com>
2018-11-06 20:41:09 +0000

    added index to show SeeClickFix issues

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,2 +1,8 @@
 class FiltersController < ApplicationController
+    
+    def index
+        # ideally this will use the latitude and longitude of the users current location, but temporarily I am supplying those details
+        json_response(Filter.show_location_issues(35.787743, -78.644257)) # this returns all the SeeClickFix issues for those coordinates
+    end
+    
 end
