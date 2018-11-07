aa305e032c5797000e5e8c7da1e6f3f062ff1a1a
Crystal Williams Brown <crystal.williams.brown@gmail.com>
2018-11-07 23:02:26 +0000

    added sidewalk filter

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,14 +1,22 @@
 class FiltersController < ApplicationController
-    
+    before_action :show_issues, only: [:index]
+    before_action :show_location_issues, only: [:location]
   def index
     # ideally this will use the latitude and longitude of the users current location, but temporarily it is showing issues for all locations
-    json_response(Filter.show_issues) # this returns all the SeeClickFix issues for those coordinates
+    json_response(@issues) # this returns all the SeeClickFix issues for those coordinates
   end
     
   def location
-    # I am limiting the location to Raleigh, but I will permit params for latitude and longitude once I get the data better filtered
-    # so it doesn't take so long to load
-    json_response(Filter.show_location_issues) # this returns all the SeeClickFix issues for those coordinates
+    # I am limiting the location to Raleigh for now but it will take coordinates in the future
+    json_response(@location_issues) # this returns all the SeeClickFix issues for those coordinates
   end
   
+  private
+  def show_issues
+    @issues = Filter.show_issues
+  end
+  
+  def show_location_issues
+    @location_issues = Filter.show_location_issues
+  end
 end
