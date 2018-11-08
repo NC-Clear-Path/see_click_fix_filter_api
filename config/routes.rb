9cdc542ef348412419907c860c497e8cb3d534f8
Crystal Williams Brown <crystal.williams.brown@gmail.com>
2018-11-08 19:33:37 +0000

    removed unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'filters#index'
   get '/filters/location' => 'filters#location'
-  get '/filters/full_location_issues' => 'filters#full_location_issues'
   resources :filters
   
 end
